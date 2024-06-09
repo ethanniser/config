62662cba5500221a0c9fd07a511693b94f0b7d9d
Ethan Niser <ethan@niser.com>
2024-06-08 19:10:29 -0700

    113   2024-06-08 19:10:25   (current)

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -3,9 +3,7 @@ _: {
     enable = true;
     shellIntegration.enableZshIntegration = true;
     settings = {
-      font_family = "MesloLGM Nerd Font";
-      italic_font = "MesloLGM Nerd Font Italic";
-      bold_font = "MesloLGM Nerd Font Bold";
+      font_family = "MesloLGS Nerd Font";
     };
     extraConfig = ''
     '';
