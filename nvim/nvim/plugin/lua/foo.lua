b2a93cc2df850bedef685a1cc95188e43a636921
Ethan Niser <ethan@niser.com>
2024-08-04 23:04:21 -0500

    gut stock plugins

---
+++ nvim/nvim/plugin/lua/foo.lua
@@ -0,0 +1,1 @@
+vim.g.foo = 'bar'
--- nvim/nvim/plugin/plugins.lua
+++ nvim/nvim/plugin/plugins.lua
@@ -3,7 +3,4 @@ if vim.g.did_load_plugins_plugin then
 end
 vim.g.did_load_plugins_plugin = true
 
--- many plugins annoyingly require a call to a 'setup' function to be loaded,
--- even with default configs
 
-require('nvim-surround').setup()
