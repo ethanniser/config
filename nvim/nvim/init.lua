3276b060e6a534f273e1ee37afcf3c6298607db1
Ethan Niser <ethan@niser.com>
2024-08-04 22:37:18 -0500

    small lua formatting

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -94,11 +94,11 @@ vim.diagnostic.config {
 
 g.editorconfig = true
 
-vim.opt.colorcolumn = '100'
+opt.colorcolumn = '100'
 
 -- Native plugins
 cmd.filetype('plugin', 'indent', 'on')
 cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
-vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
+g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
