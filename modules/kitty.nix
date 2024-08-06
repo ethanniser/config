f92bf8208a787fe4c0840648da0e64dfa80e3e5d
Ethan Niser <ethan@niser.com>
2024-08-06 06:28:34 -0500

    256   2024-08-06 06:28:29   (current)

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -4,6 +4,7 @@ _: {
     shellIntegration.enableZshIntegration = true;
     settings = {
       font_family = "MesloLGS Nerd Font";
+      macos_option_as_alt = "yes";
     };
     extraConfig = ''
     '';
