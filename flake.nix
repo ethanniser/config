8ccbeed9098400d38f448abbee63f91d0711b8c0
Ethan Niser <ethan@niser.com>
2024-08-12 22:41:19 -0500

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     # Where we get most of our software. Giant mono repo with recipes
     # called derivations that say how to build software.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # nixos-22.11
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
@@ -18,7 +18,7 @@
 
     get-flake.url = "github:ursi/get-flake";
 
-    # Local neovim flake
+    # Neovim flake
     nvim.url = "github:ethanniser/nvim.nix";
     nvim.inputs.nixpkgs.follows = "nixpkgs";
   };
