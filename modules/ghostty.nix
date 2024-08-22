aed7f1829ec33b25f644c2758b8ceb9013edd911
Ethan Niser <ethan@niser.com>
2024-08-22 11:55:13 -0400

    309   2024-08-22 11:55:09   (current)

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -4,9 +4,6 @@ _: {
     font-family = MesloLGS Nerd Font
     font-size = 14
 
-    background-opacity = 0.95
-    background-blur-radius = 20
-
     shell-integration = zsh
     macos-option-as-alt = true
     mouse-hide-while-typing = true
