e7deed58318665fcb7be0ca5e0f8aadd82849250
Ethan Niser <ethan@niser.com>
2024-08-22 10:47:38 -0400

    296   2024-08-22 10:47:34   (current)

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,5 +1,12 @@
 _: {
   xdg.configFile."ghostty/config".text = ''
+    # theme
     theme = Monokai Remastered
+
+    # fonts
+    font-family = MesloLGS Nerd Font
+    font-size = 14
+
+    macos-option-as-alt = true
   '';
 }
