d8ea6f0c7311d8a7db8930e733337bdc11c59314
Ethan Niser <ethan@niser.com>
2024-12-03 11:43:10 -0500

    remove karabiner module setting config file

--- modules/karabiner/default.nix
+++ modules/karabiner/default.nix
@@ -1,3 +1,4 @@
 _: {
-  xdg.configFile."karabiner/karabiner.json".text = builtins.readFile ./karabiner.json;
+  # idk why but this is really annoying, I guess I can just keep it here if I ever loose the file or something
+  # xdg.configFile."karabiner/karabiner.json".text = builtins.readFile ./karabiner.json;
 }
