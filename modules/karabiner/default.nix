7be83050c555c6bd8f64116a2dfc7137bb68bdd3
Ethan Niser <ethan@niser.com>
2025-05-12 12:24:16 -0700

    redisble karabiner

--- modules/karabiner/default.nix
+++ modules/karabiner/default.nix
@@ -1,4 +1,4 @@
 _: {
   # idk why but this is really annoying, I guess I can just keep it here if I ever loose the file or something
-  xdg.configFile."karabiner/karabiner.json".text = builtins.readFile ./karabiner.json;
+  # xdg.configFile."karabiner/karabiner.json".text = builtins.readFile ./karabiner.json;
 }
